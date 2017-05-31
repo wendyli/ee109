45bc6cc16d865998b3d848d965d98098ad6d118e
wendyli <liwy52395@gmail.com>
2017-05-31 00:35:04 -0700

    Update FinalProject.scala

--- spatial-quickstart-master/src/FinalProject.scala
+++ spatial-quickstart-master/src/FinalProject.scala
@@ -2,23 +2,39 @@ import spatial._
 import org.virtualized._
 import spatial.targets.DE1
 
-object StreamingSobel extends SpatialApp { 
+object StreamingSobel extends SpatialApp {
   import IR._
-
   override val target = DE1
-
-  val Kh = 3
-  val Kw = 3
+  
   val Cmax = 128
+  val BallCount = 20
+  val DoubleCount = 40
 
+  type bit   = FixPt[FALSE,_1,_0]
   type Int16 = FixPt[TRUE,_16,_0]
   type UInt8 = FixPt[FALSE,_8,_0]
   type UInt5 = FixPt[FALSE,_5,_0]
   type UInt6 = FixPt[FALSE,_6,_0]
+
   @struct case class Pixel24(b: UInt8, g: UInt8, r: UInt8)
   @struct case class Pixel16(b: UInt5, g: UInt6, r: UInt5)
+  @struct case class Circle(x: Int, y: Int, rad: Int, velx: Int, vely: Int)
+  @struct case class Position(b0: bit, b1: bit, b2: bit, b3: bit, b4: bit, b5: bit, b6: bit, b7:bit)
 
+  // Returns true if a collision occurs
   @virtualize
+  def checkCollision(ballA: Circle, ballB: Circle): Unit = {
+    val xDelta = ballA.x - ballB.x
+    val yDelta = ballA.y - ballB.y
+    val distSqr = xDelta*xDelta + yDelta*yDelta
+    val sumRad = ballA.rad + ballB.rad
+    val sqrRad = sumRad*sumRad
+
+    val output = mux((distSqr <= sqrRad), 1.to[bit], 0.to[bit])
+
+  }
+
+ @virtualize
   def convolveVideoStream(rows: Int, cols: Int): Unit = {
     val R = ArgIn[Int]
     val C = ArgIn[Int]
@@ -29,103 +45,95 @@ object StreamingSobel extends SpatialApp {
     val imgOut = StreamOut[Pixel16](target.VGA)
 
     Accel {
-      val kh = RegFile[Int16](Kh, Kw)
-      val kv = RegFile[Int16](Kh, Kw)
-
-      Pipe {
-        kh(0, 0) = 1
-        kh(1, 0) = 2
-        kh(2, 0) = 1
-        kh(0, 1) = 0
-        kh(1, 1) = 0
-        kh(2, 1) = 0
-        kh(0, 2) = -1
-        kh(1, 2) = -2
-        kh(2, 2) = -1
-        kv(0, 0) = 1
-        kv(0, 1) = 2
-        kv(0, 2) = 1
-        kv(1, 0) = 0
-        kv(1, 1) = 0
-        kv(1, 2) = 0
-        kv(2, 0) = -1
-        kv(2, 1) = -2
-        kv(2, 2) = -1
-      }
-
-      val sr = RegFile[Int16](Kh, Kw)
       val fifoIn = FIFO[Int16](128)
+      val fifo2  = FIFO[Int16](128)
       val fifoOut = FIFO[Int16](128)
-      val lb = LineBuffer[Int16](Kh, Cmax)
 
       Stream(*) { _ =>
-        // Main Hardware block here
-        // TODO: Get a pixel from imgIn, and convert it to gray scale. We will be convolving our kernel over the gray-scale pixels.
-        // After converting the pixel, you will need to push it to fifoIn.
-        // Using a fifo ensures that the timing is synchronized between the convolution stage and pixel preprocessing stage.
-        // YOUR CODE HERE:
-          val pixel_video = imgIn.value()
- 
-          val pixel_gray = (pixel_video.b.to[Int16] + pixel_video.g.to[Int16] + pixel_video.r.to[Int16]) / 3
-          fifoIn.enq(pixel_gray) 
-        // TODO: We will need to iterate over all the pixels in the frame using a Foreach loop. In each iteration of the loop,
-         // we need to convolve a Kh by Kw window around the currrent pixel with the kernel. 
-         // To obtain the window, first we need to update the line buffer by dequeuing one pixel from fifoIn and enqueuing it 
-         //into the line buffer. After the update, we can load the current window from the line buffer by using: 
-         // i => sr(i, *) <<= lb(i, c), where i is the row number and lb is the lin  buffer. 
-         //You will need to iterate over rows in the kernel to perform the load. 
-
-    
-        // YOUR CODE HERE:
-          Foreach(0 until R, 0 until C) { (r_inx, c_inx) =>
-
-            val curr_pixel = fifoIn.deq()
-
-            lb.enq(curr_pixel)
-
-            Foreach(0 until Kh par Kw) { i =>
-              sr(i, *) <<= lb(i, c_inx)
-            }
-
-        // TODO: After we load the window around the current pixel, we need to perform convolution. 
-        //If you perform the last step correctly, sr should now contain a window of size Kh by Kw. 
-        //Please convolve it with the horizontal kernel (kh) and the vertical kernel (kv).
-        // Here is an example to convolve the horizontal window with sr:
-        //  val horz = Reduce(Reg[Int16])(Kh by 1, Kw by 1) { (i, j) =>
-        //    val number = mux(r < Kh-1 || c < Kw-1, 0.to[Int16], sr(i, j))
-        //    number * kh(i, j)
-        //  }{_+_}
-        // What we are doing here is that for each pixel in the window, we multiply it by 0 if it does not overlap with the kernel.
-        // Otherwise we multiply it with the corresponding kernel value at position (i,j). 
-        // You will need to do the same computation for the vertical kernel.
-        // YOUR CODE HERE:
-  
-        val horz = Reduce(Reg[Int16])(Kh by 1, Kw by 1) { (i, j) =>
-             val number = mux(r_inx < Kh-1 || c_inx < Kw-1, 0.to[Int16], sr(i, j))
-              number * kh(i, j)
-          }{_+_}
-
-          val vert = Reduce(Reg[Int16])(Kh by 1, Kw by 1) { (i, j) =>
-             val number = mux(r_inx < Kh-1 || c_inx < Kw-1, 0.to[Int16], sr(i, j))
-              number * kv(i, j)
-          }{_+_}             
+        // *****************************************************
+        // Intialize ball array 
+        // *****************************************************
          
-        // TODO: After you obtain the convolved values with the horizontal kernel and the vertical kernel, 
-        //you can simply add the absolute values of both to form the final output of your sobel filter. Please enqueue it into fifoOut. 
-        // YOUR CODE HERE: 
-         fifoOut.enq( abs(horz.value) + abs(vert.value) )
+         // Generate random number
+         val rand = RegFile[Int](DoubleCount) // stores random x, y positions 
+         val randVel = RegFile[Int](DoubleCount) // stores random x, y velocities
 
+         val b0 = Reg[bit](0)
+         val b1 = Reg[bit](0)
+         val b2 = Reg[bit](0)
+         val b3 = Reg[bit](0)
+         val b4 = Reg[bit](0)
+         val b5 = Reg[bit](0)
+         val b6 = Reg[bit](0)
+         val b7 = Reg[bit](0)
 
+         //starting values
+         b0 := 1.to[bit]
+         b4 := 1.to[bit]
+         b5 := 1.to[bit]
+
+        // generate array of random values ( 0 - 127)
+        Sequential(0 until DoubleCount){ i=>
+          b0 := b0.value ^ b2.value
+          b1 := b1.value ^ b3.value
+          b2 := b2.value ^ b4.value
+          b3 := b3.value ^ b5.value
+          b4 := b4.value ^ b6.value
+          b5 := b5.value ^ b7.value
+          b6 := b6.value ^ b0.value
+          b7 := b7.value ^ b1.value  // for now ignore since test size is 128 pixels
+          
+          rand(i) = b0.value.to[Int]*64 + b1.value.to[Int]*32 + b2.value.to[Int]*16 + b3.value.to[Int]*8 + b4.value.to[Int]*4 + b5.value.to[Int]*2 + b6.value.to[Int]
+          randVel(i) = mux(b0.value.to[Int] == 1, 0 - (b1.value.to[Int]*8 + b2.value.to[Int]*4+ b3.value.to[Int]*2 + b4.value.to[Int]), b1.value.to[Int]*8 + b2.value.to[Int]*4+ b3.value.to[Int]*2 + b4.value.to[Int])
          }
 
 
-        // TODO: This step should be performed outside of the Foreach loop over the whole frame.
-        // You will need to dequeue pixels from fifOut, and send it to imgOut (The StreamOut port).
-        // YOUR CODE HERE:
-         val new_pixel = fifoOut.deq()
-    
-         imgOut := Pixel16(new_pixel(10::6).as[UInt5], new_pixel(10::5).as[UInt6], new_pixel(10::6).as[UInt5])
-       }   
+         // Generate intial circle structs
+         val arr = RegFile[Circle](BallCount)
+         Foreach(0 until DoubleCount-2 par 2){ i =>
+           arr(i/2) = Circle(rand(i), rand(i+1), 5, randVel(i), randVel(i+1))
+         }
+
+
+         // ADD EDGE DETECTION CODE HERE
+         // ADD EDGE DETECTION CODE HERE
+         val pixel_video = imgIn.value()
+         val pixel_gray = (pixel_video.b.to[Int16] + pixel_video.g.to[Int16] + pixel_video.r.to[Int16]) / 3
+         fifoIn.enq(pixel_gray)
+         // ADD EDGE DETECTION CODE HERE
+         // ADD EDGE DETECTION CODE HERE
+
+        // **********************************************************************
+        // Check if a collision has occured and determine new ball position
+        // **********************************************************************
+        
+        // **********************************************************************
+        // Take new ball positions and update pixel values
+        // **********************************************************************
+        
+        Foreach (0 until R, 0 until C) { (row, col) =>
+        val pixel = fifoIn.deq()
+        val accum = Reg[Int16](0)
+        accum := 0
+
+        // color individual balls green
+          Foreach (0 until BallCount){ i =>
+            val cir = arr(i)
+            val pix_val = mux((row - cir.x)*(row-cir.x) + (col - cir.y)*(col-cir.y) < cir.rad*cir.rad , 2016.to[Int16], 0.to[Int16])
+            val update  = accum | pix_val
+            accum := update
+          }
+
+          val ball_pixel = accum.value
+          fifoOut.enq(ball_pixel)
+        }
+
+      // send pixel values to image out
+      val new_pixel = fifoOut.deq()
+      imgOut := Pixel16(new_pixel(15::11).as[UInt5], new_pixel(10::5).as[UInt6], new_pixel(4::0).as[UInt5])
+
+
+      }
       ()
     }
   }
@@ -137,3 +145,4 @@ object StreamingSobel extends SpatialApp {
     convolveVideoStream(R, C)
   }
 }
+
