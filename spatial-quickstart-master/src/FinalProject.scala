b0850c1c0bba8db528dd0664ba6e054c5f205098
wendyli <liwy52395@gmail.com>
2017-06-03 16:19:37 -0700

    attempt 3
    Generates a few blue pixels on the left side of the screen and a green pixel

--- spatial-quickstart-master/src/FinalProject.scala
+++ spatial-quickstart-master/src/FinalProject.scala
@@ -20,28 +20,38 @@ object FinalProject extends SpatialApp {
     val d = args(0).to[Int]
     setArg(dwell, d)
 
-    
-
     Accel (*) {
 
-      val cir0 = Circle(100,100, 10, 0, 0)
-      val cir1 = Circle(40,60, 5, 0, 0)
-      val cir2 = Circle(150,150, 15, 0, 0)
+      val cir = SRAM[Circle](3)
+      // Fill array with circle values
+      Foreach(0 until 3){ i =>
+          cir(i) = Circle(100 + i*10,100 + i*10, 10, 5, i)
+      }
 
-      Foreach(0 until 3) { i => 
+      Foreach(0 until 1000) { i =>  // frame count
+
+        //1. Change circle values
+        Foreach(0 until 3){i=>
+          val new_cir = cir(i)
+          cir(i) = Circle(new_cir.x + new_cir.velx , new_cir.y + new_cir.vely, 10, 5, i)
+        } 
+
+        //2. Draw in circles 
         Foreach(0 until dwell) { _ =>
           Foreach(0 until Rmax, 0 until Cmax){ (r, c) =>
             
-            val pixel1 = mux((r - cir0.x)*(r -cir0.x) + (c - cir0.y)*(c -cir0.y) < cir0.rad * cir0.rad, Pixel16(0,63,0), Pixel16(0,0,0))
-            val pixel2 = mux((r - cir1.x)*(r -cir1.x) + (c - cir1.y)*(c -cir1.y) < cir1.rad * cir1.rad, Pixel16(31,0,0), Pixel16(0,0,0))
-            val pixel3 = mux((r - cir2.x)*(r -cir2.x) + (c - cir2.y)*(c -cir2.y) < cir2.rad * cir2.rad, Pixel16(0,0,31), Pixel16(0,0,0))
+            val pixel1 = mux((r - cir(0).x)*(r -cir(0).x) + (c - cir(0).y)*(c -cir(0).y) < cir(0).rad * cir(0).rad, Pixel16(0,63,0), Pixel16(0,0,0))
+            val pixel2 = mux((r - cir(1).x)*(r -cir(1).x) + (c - cir(1).y)*(c -cir(1).y) < cir(1).rad * cir(1).rad, Pixel16(31,0,0), Pixel16(0,0,0))
+            val pixel3 = mux((r - cir(2).x)*(r -cir(2).x) + (c - cir(2).y)*(c -cir(2).y) < cir(2).rad * cir(2).rad, Pixel16(0,0,31), Pixel16(0,0,0))
             
             val pixel = Pixel16(pixel1.b|pixel2.b|pixel3.b, pixel1.g|pixel2.g|pixel3.g, pixel1.r|pixel2.r|pixel3.r)
             imgOut(r, c) = pixel
 
           }
-        }
-      }
+        } 
+
+
+      } // end of frame count
     }
   }
 
