d06c07be52a1940c7fe27451224e4e267d2c6904
wendyli <liwy52395@gmail.com>
2017-06-05 10:26:15 -0700

    Scala simulation
    Prints out 
    Cir x 100
    Cir y 100
    Cir rad 10
    
    correctly reading from SRAM

--- spatial-quickstart-master/src/FinalProject.scala
+++ spatial-quickstart-master/src/FinalProject.scala
@@ -15,43 +15,37 @@ object FinalProject extends SpatialApp {
 
   @virtualize
   def convolveVideoStream(): Unit = {
-    val imgOut = BufferedOut[Pixel16](target.VGA)
+    //val imgOut = BufferedOut[Pixel16](target.VGA)
     val dwell = ArgIn[Int]
     val d = args(0).to[Int]
     setArg(dwell, d)
 
-    Accel (*) {
+    Accel {
 
       val cir = SRAM[Circle](3)
       // Fill array with circle values
-      Foreach(0 until 3){ i =>
-          cir(i) = Circle(100 + i*10,100 + i*10, 10, 5, i)
+      Foreach(0 until 1){ i =>
+          cir(i) = Circle(100 ,100, 10, 0, 0)
       }
 
-      Foreach(0 until 1000) { i =>  // frame count
-
-        //1. Change circle values
-        Foreach(0 until 3){i=>
-          val new_cir = cir(i)
-          cir(i) = Circle(new_cir.x + new_cir.velx , new_cir.y + new_cir.vely, 10, 5, i)
-        } 
-
-        //2. Draw in circles 
+      // Generate circle
+      Foreach(0 until 30000) { _ => 
         Foreach(0 until dwell) { _ =>
           Foreach(0 until Rmax, 0 until Cmax){ (r, c) =>
             
-            val pixel1 = mux((r - cir(0).x)*(r -cir(0).x) + (c - cir(0).y)*(c -cir(0).y) < cir(0).rad * cir(0).rad, Pixel16(0,63,0), Pixel16(0,0,0))
-            val pixel2 = mux((r - cir(1).x)*(r -cir(1).x) + (c - cir(1).y)*(c -cir(1).y) < cir(1).rad * cir(1).rad, Pixel16(31,0,0), Pixel16(0,0,0))
-            val pixel3 = mux((r - cir(2).x)*(r -cir(2).x) + (c - cir(2).y)*(c -cir(2).y) < cir(2).rad * cir(2).rad, Pixel16(0,0,31), Pixel16(0,0,0))
+            val pixel = mux((r - cir(0).x)*(r -cir(0).x) + (c - cir(0).y)*(c -cir(0).y) < cir(0).rad * cir(0).rad, Pixel16(0,63,0), Pixel16(0,0,0))
             
-            val pixel = Pixel16(pixel1.b|pixel2.b|pixel3.b, pixel1.g|pixel2.g|pixel3.g, pixel1.r|pixel2.r|pixel3.r)
-            imgOut(r, c) = pixel
+            println("Cir x " + cir(0).x)
+            println("Cir y " + cir(0).y)
+            println("Cir rad " + cir(0).rad)
+
+            //imgOut(r, c) = pixel
 
           }
-        } 
+        }
+      }
 
 
-      } // end of frame count
     }
   }
 
