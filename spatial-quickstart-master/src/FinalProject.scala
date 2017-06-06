821f75ce463d4e411a6139cd95148fa06cac9410
wendyli <liwy52395@gmail.com>
2017-06-06 03:47:52 -0700

    attempt - border collision
    Trying to manipulate sequentials, not working.  Ball position is still oscillating between two positions.

--- spatial-quickstart-master/src/FinalProject.scala
+++ spatial-quickstart-master/src/FinalProject.scala
@@ -10,9 +10,8 @@ object FinalProject extends SpatialApp {
   val Rmax = 240
   val BallCount = 10
 
-  type UInt8 = FixPt[FALSE,_8,_0]
-
   @struct case class Pixel16(b: UInt5, g: UInt6, r: UInt5)
+  
   @struct case class Circle(x: Int, y: Int, rad: Int, velx: Int, vely: Int)
 
   @virtualize
@@ -33,11 +32,11 @@ object FinalProject extends SpatialApp {
       // Fill array with circle values
       Foreach(0 until 3){ i =>
 
-          cirX(i)    = random[UInt8](255).to[Int]
-          cirY(i)    = random[UInt8](Rmax).to[Int]
+          cirX(i)    = 10.to[Int]
+          cirY(i)    = 10.to[Int]
           cirRad(i)  = 10.to[Int]
-          cirVelX(i) = random[UInt8](3).to[Int]
-          cirVelY(i) = random[UInt8](3).to[Int]
+          cirVelX(i) = 1.to[Int]
+          cirVelY(i) = 1.to[Int]
       }
 
       // Generate circles
@@ -45,17 +44,19 @@ object FinalProject extends SpatialApp {
 
         //Get new coordinates 
         Sequential{
-      
+        
           // Calculate new velocity vectors
-          val RCollide = mux(cirX(0) + cirRad(0) > Cmax, 1.to[Int], 0.to[Int])
-          val LCollide = mux(cirX(0) - cirRad(0) < Cmax, 1.to[Int], 0.to[Int])
-          val TCollide = mux(cirY(0) + cirRad(0) > Rmax, 1.to[Int], 0.to[Int])
-          val BCollide = mux(cirY(0) - cirRad(0) < Rmax, 1.to[Int], 0.to[Int])
+          val RCollide = mux(cirX(0) + cirRad(0) > Cmax, true, false)
+          val LCollide = mux(cirX(0) - cirRad(0) < Cmax, true, false)
+          val TCollide = mux(cirY(0) + cirRad(0) > Rmax, true, false)
+          val BCollide = mux(cirY(0) - cirRad(0) < Rmax, true, false)
 
           // Set new velocities
-          cirVelX(0) = mux( RCollide == 1.to[Int]|| LCollide == 1.to[Int], 0 - cirVelX(0), cirVelX(0))
-          cirVelY(0) = mux( TCollide == 1.to[Int]|| BCollide == 1.to[Int], 0 - cirVelY(0), cirVelY(0))
+          cirVelX(0) = mux(RCollide || LCollide, 0 - cirVelX(0), cirVelX(0))
+          cirVelY(0) = mux(TCollide || BCollide, 0 - cirVelY(0), cirVelY(0))
+        }
 
+        Sequential{
           // Calculate new positions
           cirX(0) = mux( cirX(0) + cirVelX(0) > Cmax -10, Cmax - 10, cirX(0) + cirVelX(0))
           cirY(0) = mux( cirY(0) + cirVelY(0) > Rmax -10, Rmax - 10, cirY(0) + cirVelY(0))
